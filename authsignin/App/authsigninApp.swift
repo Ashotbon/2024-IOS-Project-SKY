676704db8b990e93d5215b30e78b0481b1353ef6
Ashotbon <154383170+Ashotbon@users.noreply.github.com>
2024-03-28 09:25:25 -0400

    Connectivity

--- authsignin/App/authsigninApp.swift
+++ authsignin/App/authsigninApp.swift
@@ -1,30 +1,4 @@
-//
-//  auth_signinApp.swift
-//  auth_signin
-//
-//  Created by Ashot Harutyunyan on 2024-03-22.
-//
 
-
-//---------------------//
-//import SwiftUI
-//import Firebase
-//@main
-//struct authsigninApp: App {
-//    @StateObject var viewModel = AuthViewModel()
-//    
-//    init() {
-//        FirebaseApp.configure()
-//    }
-//    var body: some Scene {
-//        WindowGroup {
-//         ContentView()
-//                .environmentObject(viewModel)
-//        }
-//    }
-//}
-
-//---------------------//
 import SwiftUI
 import Firebase
 @main
